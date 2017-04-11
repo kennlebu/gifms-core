73c9d7ef172d207b81cc11460820664546e466a2
Nditika <mwangikevinn@gmail.com>
2017-04-11 21:11:00 +0300

    migrations

--- config/database.php
+++ config/database.php
@@ -66,6 +66,20 @@ return [
             'engine' => null,
         ],
 
+        'mysqlmg' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -77,6 +91,16 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DBSQLSVR_HOST', 'localhost'),
+            'database' => env('DBSQLSVR_DATABASE', 'forge'),
+            'username' => env('DBSQLSVR_USERNAME', 'forge'),
+            'password' => env('DBSQLSVR_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
         ],
 
     ],
