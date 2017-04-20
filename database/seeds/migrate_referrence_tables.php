2a235ee1fba0daeb64ebc196a0d2c89a87b617e0
Nditika <mwangikevinn@gmail.com>
2017-04-20 10:52:10 +0300

    refferences migrations

--- database/seeds/migrate_referrence_tables.php
+++ database/seeds/migrate_referrence_tables.php
@@ -181,6 +181,53 @@ class migrate_referrence_tables extends Seeder
 
 
 
+        /**
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         *                  Regions
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->setFetchMode(PDO::FETCH_ASSOC);
+
+        $data = DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->table('Regions')->get();
+
+        $data_to_migrate=array();
+
+        foreach ($data as $key => $value) {
+
+            $data_to_migrate[$key]['region_name']      = $data[$key]['Region'];
+            $data_to_migrate[$key]['migration_id']      = $data[$key]['ID'];
+
+
+            echo "\n Regions---";
+            echo $data[$key]['Region'];
+        }
+        
+        echo "\n-----------------------------------------------------------------------------------------------------\n";
+
+        DB::table('regions')->insert($data_to_migrate);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -219,6 +266,118 @@ class migrate_referrence_tables extends Seeder
         echo "\n-----------------------------------------------------------------------------------------------------\n";
 
         DB::table('cash_request_purposes')->insert($data_to_migrate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /**
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         *                  ExchangeRates
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->setFetchMode(PDO::FETCH_ASSOC);
+
+        $data = DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->table('ExchangeRates')->get();
+
+        $data_to_migrate=array();
+
+        foreach ($data as $key => $value) {
+
+            $data_to_migrate[$key]['exchange_rate']                 = $data[$key]['ExchangeRate'];
+            $data_to_migrate[$key]['current_rate']                  = $data[$key]['CurrRate'];
+            $data_to_migrate[$key]['active_date']                   = $data[$key]['ActiveDate'];
+            $data_to_migrate[$key]['end_date']                      = $data[$key]['EndDate'];
+            $data_to_migrate[$key]['migration_id']                  = $data[$key]['ID'];
+
+
+            echo "\n ExchangeRates---";
+            echo $data[$key]['ExchangeRate'];
+        }
+        
+        echo "\n-----------------------------------------------------------------------------------------------------\n";
+
+        DB::table('exchange_rates')->insert($data_to_migrate);
+
+
+
+
+
+
+
+        /**
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         *                  Stations
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->setFetchMode(PDO::FETCH_ASSOC);
+
+        $data = DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->table('Stations')->get();
+
+        $data_to_migrate=array();
+
+        foreach ($data as $key => $value) {
+
+            $data_to_migrate[$key]['station_name']                  = $data[$key]['Station'];
+            $data_to_migrate[$key]['logo']                          = $data[$key]['Logo'];
+            $data_to_migrate[$key]['address']                       = $data[$key]['Address'];
+            $data_to_migrate[$key]['location']                      = $data[$key]['StationAdd'];
+            $data_to_migrate[$key]['country']                       = $data[$key]['Country'];
+            $data_to_migrate[$key]['budget_default']                = $data[$key]['BudgetDefault'];
+            $data_to_migrate[$key]['telephone']                     = $data[$key]['Telephone'];
+            $data_to_migrate[$key]['mobile']                        = $data[$key]['Mobile'];
+            $data_to_migrate[$key]['primary_contact']               = $data[$key]['PrimaryContact'];
+            $data_to_migrate[$key]['email']                         = $data[$key]['EmailAddress'];
+            $data_to_migrate[$key]['licence_level']                 = $data[$key]['licenselevel'];
+            $data_to_migrate[$key]['town']                          = $data[$key]['Town'];
+            $data_to_migrate[$key]['currency']                      = $data[$key]['currncy'];
+            $data_to_migrate[$key]['expiry_date']                   = $data[$key]['ExpiryDate'];
+            $data_to_migrate[$key]['licence_status']                = $data[$key]['LicenseStatus'];
+            $data_to_migrate[$key]['accountant_email']              = $data[$key]['AccountantEmail'];
+            $data_to_migrate[$key]['finance_email']                 = $data[$key]['FinanceEmail'];
+            $data_to_migrate[$key]['manager_email']                 = $data[$key]['ManagerEmail'];
+            $data_to_migrate[$key]['installation_path']             = $data[$key]['InstallationPath'];
+            $data_to_migrate[$key]['alert_email_address']           = $data[$key]['AlertEmailAddress'];
+            $data_to_migrate[$key]['alert_email_password']          = $data[$key]['AlertEmailPassword'];
+            $data_to_migrate[$key]['migration_id']                  = $data[$key]['STID'];
+
+
+            echo "\n Stations---";
+            echo $data[$key]['StationAdd'];
+        }
+        
+        echo "\n-----------------------------------------------------------------------------------------------------\n";
+
+        DB::table('stations')->insert($data_to_migrate);
 
 
 
