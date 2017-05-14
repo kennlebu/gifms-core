afa62747e2397cb0ce7077e8a8aa105934620a41
Nditika <mwangikevinn@gmail.com>
2017-05-14 20:44:48 +0300

    #put lpo

--- app/Models/LPOModels/LpoQuotation.php
+++ app/Models/LPOModels/LpoQuotation.php
@@ -6,9 +6,25 @@ namespace App\Models\LPOModels;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\BaseModels\BaseModel;
+use App\Models\SuppliesModels\Supplier;
+use App\Models\StaffModels\Staff;
 
 class LpoQuotation extends BaseModel
 {
     //
     use SoftDeletes;
+
+    protected $appends = ['supplier','uploaded_by'];
+
+
+    public function getSupplierAttribute()
+    {
+        return Supplier::find($this->attributes['supplier_id']);
+
+    }
+    public function getUploadedByAttribute()
+    {
+        return Staff::find($this->attributes['uploaded_by_id']);
+
+    }
 }
