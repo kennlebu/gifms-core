053c57fb0ee093ea8096b17ee9668563d54b0381
Kevin Nditika <mwangikevinn@gmail.com>
2017-05-25 09:41:00 +0300

    #migration bugfix

--- database/migrations/2017_05_19_082729_create_emails_table.php
+++ database/migrations/2017_05_19_082729_create_emails_table.php
@@ -15,7 +15,6 @@ class CreateEmailsTable extends Migration
     {
         Schema::create('emails', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
             $table->text('to')->nullable();      
             $table->text('cc')->nullable();       
             $table->text('bcc')->nullable();   
@@ -26,7 +25,6 @@ class CreateEmailsTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->softDeletes();
-        });
         });
     }
 
