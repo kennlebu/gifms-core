06d9855a87aa0ddc9a65c68a8c2d2767f2ef73a1
Kevin Nditika <mwangikevinn@gmail.com>
2017-06-10 21:39:16 +0300

    #bugfix

--- config/app.php
+++ config/app.php
@@ -172,7 +172,7 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Anchu\Ftp\FtpServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        niklasravnsborg\LaravelPdf\PdfServiceProvider::class
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
 
         /*
          * Application Service Providers...
