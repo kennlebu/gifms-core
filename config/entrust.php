d508ef4f2189a514231ba475483c9e6450bf86e2
Kevin Nditika <mwangikevinn@gmail.com>
2017-06-23 00:02:05 +0300

    #entrust migration

--- config/entrust.php
+++ config/entrust.php
@@ -84,7 +84,7 @@ return [
     | between permissions and roles to the database.
     |
     */
-    'permission_role_table' => 'permission_role',
+    'permission_role_table' => 'role_permissions',
 
     /*
     |--------------------------------------------------------------------------
@@ -95,7 +95,7 @@ return [
     | database.
     |
     */
-    'role_user_table' => 'role_user',
+    'role_user_table' => 'user_roles',
 
 
 ];
