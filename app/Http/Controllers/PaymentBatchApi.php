3339c08a2173315f06013a3f9a214ba54c152cd2
Kevin Nditika <mwangikevinn@gmail.com>
2017-07-10 13:55:03 +0300

    #payment batch api

--- app/Http/Controllers/PaymentBatchApi.php
+++ app/Http/Controllers/PaymentBatchApi.php
@@ -16,6 +16,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\PaymentModels\PaymentBatch;
 
 class PaymentBatchApi extends Controller
 {
@@ -239,15 +241,242 @@ class PaymentBatchApi extends Controller
      */
     public function getPaymentBatches()
     {
+
+
         $input = Request::all();
+        //query builder
+        $qb = DB::table('payment_batches');
 
-        //path params validation
+        $qb->whereNull('deleted_at');
+
+        $response;
+        $response_dt;
+
+        $total_records          = $qb->count();
+        $records_filtered       = 0;
 
 
-        //not path params validation
-        $payment_batch_id = $input['payment_batch_id'];
+
+        // $qb->where('requested_by_id',$this->current_user()->id);
 
 
-        return response('How about implementing getPaymentBatches as a GET method ?');
+
+        //searching
+        if(array_key_exists('searchval', $input)){
+            $qb->where(function ($query) use ($input) {
+                
+                $query->orWhere('id','like', '\'%' . $input['searchval']. '%\'');
+                $query->orWhere('ref','like', '\'%' . $input['search']['value']. '%\'');
+                // $query->orWhere('payable_type','like', '\'%' . $input['searchval']. '%\'');
+                // $query->orWhere('paid_to_bank_account_no','like', '\'%' . $input['searchval']. '%\'');
+                // $query->orWhere('paid_to_name','like', '\'%' . $input['searchval']. '%\'');
+                // $query->orWhere('payment_desc','like', '\'%' . $input['searchval']. '%\'');
+
+            });
+
+            // $records_filtered       =  $qb->count(); //doesn't work
+
+            $sql = PaymentBatch::bind_presql($qb->toSql(),$qb->getBindings());
+            $sql = str_replace("*"," count(*) AS count ", $sql);
+            $dt = json_decode(json_encode(DB::select($sql)), true);
+
+            $records_filtered = (int) $dt[0]['count'];
+            // $records_filtered = 30;
+
+
+        }
+
+
+
+
+        if(array_key_exists('datatables', $input)){
+
+            //searching
+            $qb->where(function ($query) use ($input) {
+                
+                $query->orWhere('id','like', '\'%' . $input['search']['value']. '%\'');
+                $query->orWhere('ref','like', '\'%' . $input['search']['value']. '%\'');
+                // $query->orWhere('payable_type','like', '\'%' . $input['search']['value']. '%\'');
+                // $query->orWhere('paid_to_bank_account_no','like', '\'%' . $input['search']['value']. '%\'');
+                // $query->orWhere('paid_to_name','like', '\'%' . $input['search']['value']. '%\'');
+                // $query->orWhere('payment_desc','like', '\'%' . $input['search']['value']. '%\'');
+
+            });
+
+
+
+
+            $sql = PaymentBatch::bind_presql($qb->toSql(),$qb->getBindings());
+            $sql = str_replace("*"," count(*) AS count ", $sql);
+            $dt = json_decode(json_encode(DB::select($sql)), true);
+
+            $records_filtered = (int) $dt[0]['count'];
+
+
+            //ordering
+            $order_column_id    = (int) $input['order'][0]['column'];
+            $order_column_name  = $input['columns'][$order_column_id]['order_by'];
+            $order_direction    = $input['order'][0]['dir'];
+
+
+
+            // if ($order_column_id == 0){
+            //     $order_column_name = "created_at";
+            // }
+            // if ($order_column_id == 1){
+            //     $order_column_name = "id";
+            // }
+
+            if($order_column_name!=''){
+
+                $qb->orderBy($order_column_name, $order_direction);
+
+            }
+
+
+
+
+
+
+            //limit $ offset
+            $response_dt    =   $qb->limit($input['length'])->offset($input['start']);
+
+
+
+
+
+            $sql = PaymentBatch::bind_presql($qb->toSql(),$qb->getBindings());
+
+            // $response_dt = DB::select($qb->toSql(),$qb->getBindings());         //pseudo
+            $response_dt = DB::select($sql);
+
+
+            $response_dt = json_decode(json_encode($response_dt), true);
+
+            $response_dt    = $this->append_relationships_objects($response_dt);
+            $response_dt    = $this->append_relationships_nulls($response_dt);
+            $response       = PaymentBatch::arr_to_dt_response( 
+                $response_dt, $input['draw'],
+                $total_records,
+                $records_filtered
+                );
+
+
+        }else{
+
+            $sql            = PaymentBatch::bind_presql($qb->toSql(),$qb->getBindings());
+            $response       = json_decode(json_encode(DB::select($sql)), true);
+        }
+
+
+
+
+        return response()->json($response, 200,array(),JSON_PRETTY_PRINT);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function append_relationships_objects($data = array()){
+
+        // print_r($data);
+
+        foreach ($data as $key => $value) {
+
+            $mobile_payment = PaymentBatch::find($data[$key]['id']);
+
+            // $data[$key]['payable']                      = $mobile_payment->payable;
+            // $data[$key]['debit_bank_account']           = $mobile_payment->debit_bank_account;
+            // $data[$key]['currency']                     = $mobile_payment->currency;
+            // $data[$key]['paid_to_bank']                 = $mobile_payment->paid_to_bank;
+            // $data[$key]['paid_to_bank_branch']          = $mobile_payment->paid_to_bank_branch;
+            // $data[$key]['payment_mode']                 = $mobile_payment->payment_mode;
+            // $data[$key]['payment_batch']                = $mobile_payment->payment_batch;
+
+        }
+
+        return $data;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    public function append_relationships_nulls($data = array()){
+
+
+        foreach ($data as $key => $value) {
+
+            // if($value["payable"]==null){
+            //     $data[$key]['payable'] = array("expense_desc"=>"N/A");
+                
+            // }
+            // if($value["debit_bank_account"]==null){
+            //     $data[$key]['debit_bank_account'] = array("title"=>"N/A","account_number"=>"N/A");
+                
+            // }
+            // if($value["currency"]==null){
+            //     $data[$key]['currency'] = array("currency_name"=>"N/A");
+                
+            // }
+            // if($value["paid_to_bank"]==null){
+            //     $data[$key]['paid_to_bank'] = array("bank_name"=>"N/A");
+                
+            // }
+            // if($value["paid_to_bank_branch"]==null){
+            //     $data[$key]['paid_to_bank_branch'] = array("branch_name"=>"N/A");
+                
+            // }
+            // if($value["payment_mode"]==null){
+            //     $data[$key]['payment_mode'] = array("payment_mode_description"=>"N/A");
+                
+            // }
+            // if($value["payment_batch"]==null){
+            //     $data[$key]['payment_batch'] = array("ref"=>"N/A");
+                
+            // }
+
+
+        }
+
+        return $data;
+
+
     }
 }
