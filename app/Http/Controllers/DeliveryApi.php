1ae501211b89aac1a44111c721be472e1fa27a27
Kevin Nditika <mwangikevinn@gmail.com>
2017-07-18 11:38:20 +0300

    #deliveries

--- app/Http/Controllers/DeliveryApi.php
+++ app/Http/Controllers/DeliveryApi.php
@@ -51,52 +51,6 @@ class DeliveryApi extends Controller
 
 
 
-
-    /**
-     * Operation getDeliveries
-     *
-     * deliveries List.
-     *
-     *
-     * @return Http response
-     */
-    public function getDeliveries()
-    {
-        $input = Request::all();
-
-        //path params validation
-
-
-        //not path params validation
-        $delivery_id = $input['delivery_id'];
-
-
-        return response('How about implementing getDeliveries as a GET method ?');
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     /**
      * Operation addDelivery
      *
@@ -459,5 +413,296 @@ class DeliveryApi extends Controller
         //not path params validation
 
         return response('How about implementing submitDeliveryForApproval as a PATCH method ?');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Operation getDeliveries
+     *
+     * deliveries List.
+     *
+     *
+     * @return Http response
+     */
+    public function getDeliveries()
+    {
+
+
+        $input = Request::all();
+        //query builder
+        $qb = DB::table('deliveries');
+
+        $qb->whereNull('deleted_at');
+
+        $response;
+        $response_dt;
+
+        $total_records          = $qb->count();
+        $records_filtered       = 0;
+
+
+
+
+
+
+
+
+        // $qb->where('requested_by_id',$this->current_user()->id);
+
+
+
+        //searching
+        if(array_key_exists('searchval', $input)){
+            $qb->where(function ($query) use ($input) {
+                
+                $query->orWhere('id','like', '\'%' . $input['searchval']. '%\'');
+                $query->orWhere('ref','like', '\'%' . $input['search']['value']. '%\'');
+                $query->orWhere('external_ref','like', '\'%' . $input['search']['value']. '%\'');
+
+            });
+
+            // $records_filtered       =  $qb->count(); //doesn't work
+
+            $sql = Delivery::bind_presql($qb->toSql(),$qb->getBindings());
+            $sql = str_replace("*"," count(*) AS count ", $sql);
+            $dt = json_decode(json_encode(DB::select($sql)), true);
+
+            $records_filtered = (int) $dt[0]['count'];
+            // $records_filtered = 30;
+
+
+        }
+
+
+
+
+        if(array_key_exists('datatables', $input)){
+
+            //searching
+            $qb->where(function ($query) use ($input) {
+                
+                $query->orWhere('id','like', '\'%' . $input['search']['value']. '%\'');
+                $query->orWhere('ref','like', '\'%' . $input['search']['value']. '%\'');
+                $query->orWhere('external_ref','like', '\'%' . $input['search']['value']. '%\'');
+
+            });
+
+
+
+
+            $sql = Delivery::bind_presql($qb->toSql(),$qb->getBindings());
+            $sql = str_replace("*"," count(*) AS count ", $sql);
+            $dt = json_decode(json_encode(DB::select($sql)), true);
+
+            $records_filtered = (int) $dt[0]['count'];
+
+
+            //ordering
+            $order_column_id    = (int) $input['order'][0]['column'];
+            $order_column_name  = $input['columns'][$order_column_id]['order_by'];
+            $order_direction    = $input['order'][0]['dir'];
+
+
+
+            // if ($order_column_id == 0){
+            //     $order_column_name = "created_at";
+            // }
+            // if ($order_column_id == 1){
+            //     $order_column_name = "id";
+            // }
+
+            if($order_column_name!=''){
+
+                $qb->orderBy($order_column_name, $order_direction);
+
+            }
+
+
+
+
+
+
+            //limit $ offset
+            if((int)$input['start']!= 0 ){
+
+                $response_dt    =   $qb->limit($input['length'])->offset($input['start']);
+
+            }else{
+                $qb->limit($input['length']);
+            }
+
+
+
+
+
+            $sql = Delivery::bind_presql($qb->toSql(),$qb->getBindings());
+
+            // $response_dt = DB::select($qb->toSql(),$qb->getBindings());         //pseudo
+            $response_dt = DB::select($sql);
+
+
+            $response_dt = json_decode(json_encode($response_dt), true);
+
+            $response_dt    = $this->append_relationships_objects($response_dt);
+            $response_dt    = $this->append_relationships_nulls($response_dt);
+            $response       = Delivery::arr_to_dt_response( 
+                $response_dt, $input['draw'],
+                $total_records,
+                $records_filtered
+                );
+
+
+        }else{
+
+            $sql            = Delivery::bind_presql($qb->toSql(),$qb->getBindings());
+            $response       = json_decode(json_encode(DB::select($sql)), true);
+        }
+
+
+
+
+        return response()->json($response, 200,array(),JSON_PRETTY_PRINT);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function append_relationships_objects($data = array()){
+
+        // print_r($data);
+
+        foreach ($data as $key => $value) {
+
+            $delivery = Delivery::find($data[$key]['id']);
+
+            $data[$key]['lpo']                      = $delivery->lpo;
+            $data[$key]['received_by']              = $delivery->received_by;
+
+        }
+
+        return $data;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    public function append_relationships_nulls($data = array()){
+
+
+        foreach ($data as $key => $value) {
+
+            if($value["lpo"]==null){
+                $data[$key]['lpo'] = array("expense_desc"=>"N/A");
+                
+            }
+            if($value["received_by"]==null){
+                $data[$key]['received_by'] = array("full_name"=>"N/A");
+                
+            }
+
+
+        }
+
+        return $data;
+
+
     }
 }
