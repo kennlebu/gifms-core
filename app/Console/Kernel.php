a6e83f08c7b062b7cb8e8aca8eb6eebf8689e0e0
Kevin Nditika <mwangikevinn@gmail.com>
2017-08-03 13:54:53 +0300

    #register  command GenerateTransactionDocuments

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\MigrateFiles::class
+        Commands\MigrateFiles::class.
+        Commands\GenerateTransactionDocuments::class
     ];
 
     /**
