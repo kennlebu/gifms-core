f214104c6eabfc85324d7b95a17e55b9921a738e
Kevin Nditika <mwangikevinn@gmail.com>
2017-08-13 16:47:52 +0300

    #fix

--- app/Mail/SubmitClaim.php
+++ app/Mail/SubmitClaim.php
@@ -28,6 +28,6 @@ class SubmitClaim extends Mailable
      */
     public function build()
     {
-        return $this->view('mail/submit.claim');
+        return $this->view('emails/submit.claim');
     }
 }
