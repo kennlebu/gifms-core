0574ebcf61fe2fb1678eaa9aaa3ef32ad8025a5f
Kevin Nditika <mwangikevinn@gmail.com>
2017-10-12 11:52:06 +0300

    #Image not found or type unknown

--- resources/views/pdf/invoice_payment_voucher.blade.php
+++ resources/views/pdf/invoice_payment_voucher.blade.php
@@ -240,7 +240,7 @@
                         </td>
                         <td style="border-top: 1px solid #C0C0C0; border-bottom: 1px solid #C0C0C0; border-left: 1px solid #000000; border-right: 1px solid #000000;  vertical-align: top;" colspan="3" >
                             <small>signed:</small><br/>
-                            <img height = "80" src="{{asset('../storage/app/staff/signature'.$approval->approver_id.'.png')}}"></img>
+                            <img height = "60" alt="." src="{{asset('../storage/app/staff/signature'.$approval->approver_id.'.png')}}"></img>
                         </td>
                         <td style="border-top: 1px solid #C0C0C0; border-bottom: 1px solid #C0C0C0; border-left: 1px solid #000000; border-right: 1px solid #000000;   vertical-align: top;  background-color: #f7f8fb;"  colspan="2" >                  
                                 <small>Date</small><br/><br/><br/>          
