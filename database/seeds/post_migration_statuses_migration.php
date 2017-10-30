2e93e611dd75f3567f8ed997b973c2b7f9e86427
Kevin Nditika <mwangikevinn@gmail.com>
2017-10-30 05:10:12 +0300

    #mig

--- database/seeds/post_migration_statuses_migration.php
+++ database/seeds/post_migration_statuses_migration.php
@@ -16,17 +16,17 @@ class post_migration_statuses_migration extends Seeder
 
         $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='1', `display_color`='#64547a6e' WHERE `id`='11'"; 
         DB::statement($sql);
-        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='2', `display_color`='#64547a6e' WHERE `id`='10'"; 
+        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='2', `display_color`='#64547a6e', `invoice_status`='Received Pending Submission'  WHERE `id`='10'"; 
         DB::statement($sql);
-        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='3' WHERE `id`='12'"; 
+        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='3', `invoice_status`='Pending Accountant Review'  WHERE `id`='12'"; 
         DB::statement($sql);
-        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='4' WHERE `id`='1'"; 
+        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='4', `invoice_status`='Pending PM Approval' WHERE `id`='1'"; 
         DB::statement($sql);
-        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='5' WHERE `id`='2'"; 
+        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='5', `invoice_status`='Pending Finance Approval' WHERE `id`='2'"; 
         DB::statement($sql);
-        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='6' WHERE `id`='3'"; 
+        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='6', `invoice_status`='Pending Management Approval'  WHERE `id`='3'"; 
         DB::statement($sql);
-        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `next_status_id`='5', `order_priority`='7', `display_color`='##075b23a1' WHERE `id`='4'"; 
+        $sql = "UPDATE `gifms1`.`invoice_statuses` SET `next_status_id`='5', `order_priority`='7', `display_color`='##075b23a1', `invoice_status`='Pending Finance CSV Creation (Batching)'  WHERE `id`='4'"; 
         DB::statement($sql);
         $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='9', `deleted_at`='2017-10-24 12:48:44' , `display_color`='##075b23a1' WHERE `id`='6'"; 
         DB::statement($sql);
@@ -39,5 +39,55 @@ class post_migration_statuses_migration extends Seeder
         $sql = "UPDATE `gifms1`.`invoice_statuses` SET `order_priority`='12', `display_color`='#ff5555c2' WHERE `id`='9'"; 
         DB::statement($sql);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        $sql = "UPDATE `lpo_statuses` SET `deleted_at`='2017-10-24 12:48:44' WHERE `id`='1'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `order_priority`='1', `display_color`='#64547a6e' WHERE `id`='2'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `lpo_status`='Pending Accountant Review', `order_priority`='2' WHERE `id`='13'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `lpo_status`='Pending PM Approval ', `order_priority`='3' WHERE `id`='3'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `order_priority`='4' WHERE `id`='4'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `order_priority`='5' WHERE `id`='5'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `deleted_at`='2017-10-24 12:48:44' WHERE `id`='14'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `deleted_at`='2017-10-24 12:48:44' WHERE `id`='6'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `order_priority`='6', `display_color`='#075b23a1' WHERE `id`='7'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `order_priority`='7', `display_color`='#075b23a1' WHERE `id`='8'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `order_priority`='8', `display_color`='#075b23a1' WHERE `id`='9'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `deleted_at`='2017-10-24 12:48:44' WHERE `id`='10'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `display_color`='#ff5555c2',`order_priority`='10' WHERE `id`='12'"; 
+        DB::statement($sql);
+        $sql = "UPDATE `lpo_statuses` SET `display_color`='#ff5555c2',`order_priority`='9' WHERE `id`='11'"; 
+        DB::statement($sql);
+
     }
 }
+
