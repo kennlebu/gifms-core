e98dd3dec076959150bc9be22d5ced7e23c10066
Kevin Nditika <mwangikevinn@gmail.com>
2017-11-26 15:17:51 +0300

    #ywb

--- app/Models/ClaimsModels/ClaimStatus.php
+++ app/Models/ClaimsModels/ClaimStatus.php
@@ -22,4 +22,12 @@ class ClaimStatus extends BaseModel
 		        ->count();
 
     }
+    public function next_status()
+    {
+        return $this->belongsTo('App\Models\ClaimsModels\ClaimStatus','next_status_id');
+    }
+    public function approval_level()
+    {
+        return $this->belongsTo('App\Models\ApprovalsModels\ApprovalLevel','approval_level_id');
+    }
 }
--- app/Models/MobilePaymentModels/MobilePaymentStatus.php
+++ app/Models/MobilePaymentModels/MobilePaymentStatus.php
@@ -24,5 +24,13 @@ class MobilePaymentStatus extends BaseModel
 		        ->count();
 
     }
+    public function next_status()
+    {
+        return $this->belongsTo('App\Models\MobilePaymentModels\MobilePaymentStatus','next_status_id');
+    }
+    public function approval_level()
+    {
+        return $this->belongsTo('App\Models\ApprovalsModels\ApprovalLevel','approval_level_id');
+    }
 
 }
