4a6a4869d035e1b47f9616c72d2e02c6d8b01c1a
Kenneth Lebu <kenlebu@gmail.com>
2018-05-04 13:11:10 +0300

    Change management approval date migration to pick from csv

--- app/Console/Commands/MigrateMobilePaymentDates.php
+++ app/Console/Commands/MigrateMobilePaymentDates.php
@@ -8,6 +8,7 @@ use Config;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\DB;
+use Excel;
 
 class MigrateMobilePaymentDates extends Command
 {
@@ -48,6 +49,21 @@ class MigrateMobilePaymentDates extends Command
         $data = DB::connection(env('DB_MIGRATE_FROM','sqlsrv'))->table('Allowances')->get();
 
         $data_to_update=array();
+        $path = '/reports/dates2.csv';
+        $url            = storage_path("app".$path);
+        // $file           = File::get($url);
+        $handle = fopen($url, 'r');
+        $header = false;
+        while($csvLine = fgetcsv($handle, 1000, ',')){
+            if ($header) {
+                $header = false;
+            } else {
+                $row = array();
+                $row['migration_id'] = $csvLine[0];
+                $row['management_approval_at'] = $csvLine[1];
+                array_push($data_to_update, $row);
+            }
+        }
 
         foreach ($data as $key => $value) {
 
@@ -65,7 +81,7 @@ class MigrateMobilePaymentDates extends Command
         $insertBatchs = array_chunk($data_to_update, 500);
         foreach ($insertBatchs as $batch) {
             foreach($batch as $record){
-                // file_put_contents ( "C://Users//Kenn//Desktop//debug.txt" , '\nSQL:: '.json_encode($batch) , FILE_APPEND);
+                // file_put_contents ( "C://Users//Kenn//Desktop//debug.txt" , PHP_EOL.'ROW:: '.json_encode($batch) , FILE_APPEND);
                 $mp = MobilePayment::where('migration_id', $record['migration_id'])->first();
                 $mp->management_approval_at = $record['management_approval_at'];
                 $mp->save();
@@ -77,44 +93,5 @@ class MigrateMobilePaymentDates extends Command
 
         $this->info("Done!");
     }
-
-    // try{
-
-    //     $handle = fopen($file, 'r');
-    //     $header = true;
-    //     while($csvLine = fgetcsv($handle, 1000, ',')){
-    //         if ($header) {
-    //             $header = false;
-    //         } else {
-    //             $ref = explode(" ", $csvLine[1])[0];
-    //             array_push($mobile_payment_ids, (int)preg_replace("/[^0-9]/", "", $ref)); 
-    //         }
-    //     }
-    //     // Get only unique values
-    //     $mobile_payment_ids = array_unique($mobile_payment_ids);
-
-        
-
-    //     // Change status of the mobile payment(s) to paid
-    //     foreach($mobile_payment_ids as $id){
-    //         $mobile_payment = MobilePayment::findOrFail($id);
-    //         $mobile_payment->status_id = 5;
-    //         $mobile_payment->save();
-    //         array_push($payment_refs, $mobile_payment->ref);
-
-    //         // // Mark payees as paid
-    //         // foreach($mobile_payment->payees as $pid){
-    //         //     $payee = MobilePaymentPayee::findOrFail($pid);
-    //         //     $payee->paid = 1;
-    //         //     $payee->save();
-    //         // }
-    //     }
-
-    //     return Response()->json(array('msg' => 'Success: Mobile Payment(s) reconciled','payments' => $payment_refs), 200);
-
-    // }
-    // catch (Exception $e){
-    //     return response()->json(['error'=>$e->getMessage()], 500);
-    // }
 
 }
