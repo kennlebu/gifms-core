95323d80d9a729cf846c063570008c979bbe23ab
Kenneth Lebu <kenlebu@gmail.com>
2018-05-17 13:37:38 +0300

    Add all PIDs for Jane Ayuma

--- app/Http/Controllers/ProjectApi.php
+++ app/Http/Controllers/ProjectApi.php
@@ -453,7 +453,8 @@ class ProjectApi extends Controller
 
         
         //my_assigned
-        if((array_key_exists('my_assigned', $input)&& $input['my_assigned'] = "true")&&($current_user->hasRole(['accountant','assistant-accountant','financial-controller']))){
+        if((array_key_exists('my_assigned', $input)&& $input['my_assigned'] = "true")&&($current_user->hasRole(['accountant','assistant-accountant','financial-controller'])
+           || $current_user->id == 36)){// If Jane Ayuma. This is a hack. TODO: Find a way to make this dynamic
 
             $qb->whereNotNull('project_code');
         }elseif (array_key_exists('my_assigned', $input)&& $input['my_assigned'] = "true") {
