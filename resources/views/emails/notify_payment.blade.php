b6dd6e8b042331257dbdfcaf838fbb1bc9f043f8
Kenneth Lebu <kenlebu@gmail.com>
2018-09-13 10:34:44 +0300

    Add expense description to payment notification email

--- resources/views/emails/notify_payment.blade.php
+++ resources/views/emails/notify_payment.blade.php
@@ -6,24 +6,23 @@
 <body style="font-family: monospace;">
     <main>
         <div>
-            Dear {{$addressed_to}},
+            To Whom It May Concern <br/>
+            {{$addressed_to}}
             <br/>
             <br/>
             <p>
                 @if ($payable_type=='advances') 
-                    Your Advance Ref.: {{$payable->ref}} of amount {{$payable->currency->currency_name}} {{number_format($payable->total, 2)}} has been paid successfully. 
+                    Your Advance Ref.: {{$payable->ref}} for {{$payable->expense_desc}} of amount {{$payable->currency->currency_name}} {{number_format($payable->total, 2)}} has been paid successfully. 
                 @elseif ($payable_type=='claims') 
-                    Your Claim Ref.: {{$payable->ref}} of amount {{$payable->currency->currency_name}} {{number_format($payable->total, 2)}} has been paid successfully. 
+                    Your Claim Ref.: {{$payable->ref}} for {{$payable->expense_desc}} of amount {{$payable->currency->currency_name}} {{number_format($payable->total, 2)}} has been paid successfully. 
                 @elseif ($payable_type=='mobile_payments') 
-                    Your Mobile Payment Ref.: {{$payable->ref}} of amount {{$payable->currency->currency_name}} {{number_format($payable->totals, 2)}} has been paid successfully. 
+                    Your Mobile Payment Ref.: {{$payable->ref}} for {{$payable->expense_desc}} of amount {{$payable->currency->currency_name}} {{number_format($payable->totals, 2)}} has been paid successfully. 
                 @elseif ($payable_type=='invoices') 
-                    Your Invoice Ref.: {{$payable->external_ref}} of amount {{$payable->currency->currency_name}} {{number_format($payable->total, 2)}}
+                    Your Invoice Ref.: {{$payable->external_ref}} for {{$payable->expense_desc}} of amount {{$payable->currency->currency_name}} {{number_format($payable->total, 2)}}
                     has been paid successfully. 
                 @endif
 
             </p>
-            <!-- <p>Please find a copy attached for you to download and process accordingly.</p>
-<p>Please note all payments are made via electronic transfer so kindly submit your Bank Details to our offices.</p> -->
             <p>Should you have any questions, or queries on the above, please do not hesitate to get in touch with us via:<br/><br/> 3rd flr, Timau Plaza, Arwings Kodhek Road,<br/> P O Box 2011-00100 Nairobi, Kenya<br/> (t) : 254 20 514 3100/5<br/> (e) :
                 <a href="mailto:jayuma@clintonhealthaccess.org">jayuma@clintonhealthaccess.org<a/><br/>
 <a href="https://www.clintonhealthaccess.org">www.clintonhealthaccess.org</a>
