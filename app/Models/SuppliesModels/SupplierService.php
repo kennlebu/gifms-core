aefa969f8934bf2c70fd95bf740afb63d99f5407
Kenneth Lebu <kenlebu@gmail.com>
2018-10-01 19:44:53 +0300

    Same as previous commit

--- app/Models/SuppliesModels/SupplierService.php
+++ app/Models/SuppliesModels/SupplierService.php
@@ -11,4 +11,9 @@ class SupplierService extends BaseModel
     //
     use SoftDeletes;
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function supply_category()
+    {
+        return $this->belongsTo('App\Models\SuppliesModels\SupplyCategory','supply_category_id');
+    }
 }
