7e4252bd971d9f4415a60c7046d59b80d6fe6d5f
Kenneth Lebu <kenlebu@gmail.com>
2019-02-06 08:33:45 +0300

    Fix being able to book same room over an existing booking

--- app/Http/Controllers/RoomsApi.php
+++ app/Http/Controllers/RoomsApi.php
@@ -242,8 +242,18 @@ class RoomsApi extends Controller
             $to_datetime = $booking->to_date.' '.$booking->to_time;
 
             // Check if room is already booked at that time
-            $booked = DB::select("select * from meeting_room_bookings where (cast(concat(from_date, ' ', from_time) as datetime) between DATE_ADD('".$from_datetime."', INTERVAL 1 MINUTE) and DATE_SUB('".$to_datetime."', INTERVAL 1 MINUTE)
-            or cast(concat(to_date, ' ', to_time) as datetime) between DATE_ADD('".$from_datetime."', INTERVAL 1 MINUTE) and DATE_SUB('".$to_datetime."', INTERVAL 1 MINUTE)) and room_id = ".$booking->room_id." and deleted_at is null");
+            $booked = DB::select("select * from meeting_room_bookings
+            where ((DATE_ADD('".$from_datetime."', INTERVAL 1 MINUTE) between 
+            cast(concat(from_date, ' ', from_time) as datetime) and cast(concat(to_date, ' ', to_time) as datetime)
+            or DATE_SUB('".$to_datetime."', INTERVAL 1 MINUTE) between
+            cast(concat(from_date, ' ', from_time) as datetime) and cast(concat(to_date, ' ', to_time) as datetime))
+            or (cast(concat(from_date, ' ', from_time) as datetime) 
+            between DATE_ADD('".$from_datetime."', INTERVAL 1 MINUTE) and DATE_SUB('".$to_datetime."', INTERVAL 1 MINUTE)
+            or cast(concat(to_date, ' ', to_time) as datetime) 
+            between DATE_ADD('".$from_datetime."', INTERVAL 1 MINUTE) and DATE_SUB('".$to_datetime."', INTERVAL 1 MINUTE)))
+            and room_id = ".$booking->room_id." and deleted_at is null");
+
+            // file_put_contents ( "C://Users//Kenn//Desktop//debug.txt" , PHP_EOL.json_encode($booked) , FILE_APPEND);
 
             if(!empty($booked)){
                 return response()->json(array('msg' => 'already booked'), 409);
