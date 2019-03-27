684dc9e1a15611662c91f6075505c855e1694e74
Kenneth Lebu <kenlebu@gmail.com>
2019-03-27 15:05:23 +0300

    Same stupid error as previous commit

--- app/Http/Controllers/ReportsApi.php
+++ app/Http/Controllers/ReportsApi.php
@@ -161,7 +161,7 @@ class ReportsApi extends Controller
             if(($row['payable_type']=='mobile_payments' && empty($row['payable']['migration_invoice_id']))){
                 // $voucher_no = VoucherNumber::find($row['payable']['voucher_no']);
                 $voucher_no = VoucherNumber::where('payable_id', $row['payable']['id'])->first();
-                $voucher_no = $voucher->voucher_number;
+                $voucher_no = $voucher_no->voucher_number;
                 if(!empty($voucher_no->voucher_number)) $voucher_no = $voucher_no->voucher_number;
                 else $voucher_no = '-';
             }
