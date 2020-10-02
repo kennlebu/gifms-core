36aa6300a049a5510dd7abf6ed7cf59d364458b2
Kenya Finance <KEFinance@clintonhealthaccess.org>
2020-10-02 14:25:22 +0300

    Fix claim approvals (I hope)

--- app/Http/Controllers/ClaimApi.php
+++ app/Http/Controllers/ClaimApi.php
@@ -842,7 +842,8 @@ class ClaimApi extends Controller
                 'financial-controller',
                 'program-manager', 
                 'accountant', 
-                'assistant-accountant']
+                'assistant-accountant',
+                'financial-reviewer']
             )){                   
                 $qb = $qb->where(function ($query) use ($app_stat,$current_user) {
                     foreach ($app_stat as $key => $value) {
